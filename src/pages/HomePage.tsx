735261940b215baa6d010a4251fdcd4956dcda5b
Pierre Fournier <peiofour@gmail.com>
2022-03-10 17:44:02 +0100

    Add menu addboard

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -23,9 +23,16 @@ const HomePage = () => {
       </div>
       <p>JAMap est l'application qui centralise l'ensemble des panneaux d'expression libre en France. Elle va te permettre de les trouver plus facilement, d'avoir les itinéraires directement sur Waze ou Google Maps, d'avoir leur dernière date de collage, de signaler quand tu viens de coller des affiches dessus.</p>
       <h3>À vos seaux et pinceaux, on a une campagne à gagner !</h3>
-      <Button color="brown" onClick={()=>{navigate("/boards")}}>
-        Accéder à la carte
-      </Button>
+      <div>
+        <Button color="brown" onClick={()=>{navigate("/boards")}}>
+          Accéder à la carte
+        </Button>
+      </div>
+      <div style={{marginTop:"10px"}}>
+        <Button color="green" onClick={()=>{navigate("/addboard")}}>
+          Ajouter un panneau
+        </Button>
+      </div>
       <p>Le site est optimisé pour une utilisation sur smartphone.</p>
       <p>Développé par <a href="http://pierrefournier.dev" target="_blank" rel="noreferrer">Pierre Fournier</a> (<a href="https://t.me/superbasque" target="_blank" rel="noreferrer">Telegram</a>).<br/>Logo par Océane Larousse</p>
     
