92c3e5706713df3ba33c6bab348f62d597a1b7b7
Pierre Fournier <peiofour@gmail.com>
2022-03-23 21:49:23 +0100

    Fix homepage button to go to add member page.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -48,7 +48,7 @@ const HomePage = () => {
         </Button>
       </div>
       {
-        role === "admin"&&
+        role !== "member"  &&
         <div style={{marginTop:"10px"}}>
           <Button color="red" onClick={()=>{navigate("/addmember")}}>
             [Admin] Ajouter un membre
